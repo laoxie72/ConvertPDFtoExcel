d43dc1cf73d439475d32d3a08994557bd1f9505c
laoxie72 <744286652@qq.com>
2025-01-14 14:04:03 +0800

    fix:pdf->excel bug

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -3,6 +3,8 @@ import logging
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QFileDialog, QMessageBox
 )
+
+from backend.csv_to_excel import convert_csv_to_excel
 from backend.pdf_to_csv import read_table_from_pdf_by_title
 
 
@@ -44,8 +46,8 @@ class MainWindow(QMainWindow):
         self.result_text.setReadOnly(True)
 
         # 状态反馈框
-        self.status_label = QLabel("", self)
-        self.status_label.setGeometry(20, 250, 400, 30)
+        # self.status_label = QLabel("", self)
+        # self.status_label.setGeometry(20, 250, 400, 30)
 
     def select_pdf_file(self):
         """选择 PDF 文件"""
@@ -75,14 +77,19 @@ class MainWindow(QMainWindow):
 
         file_path = self.file_path_input.text()
         title = self.title_input.text().strip()
-        output_csv_path = "output.csv"  # 输出文件路径
+        # output_csv_path = "output.csv"  # 输出文件路径
 
         try:
-            # 调用后端函数
+            # 调用后端函数 PDF->CSV->Excel
+            output_csv_path = "output/output.csv"
+            output_excel_path = "output/output.xlsx"
+
             read_table_from_pdf_by_title(file_path, output_csv_path, title)
-            QMessageBox.information(self, "成功", f"表格已成功提取并保存到 {output_csv_path}！")
-            self.result_text.setText(f"表格已保存到 {output_csv_path}")
-            self.status_label.setText("转换成功！")
+            convert_csv_to_excel(output_csv_path, output_excel_path)
+
+            QMessageBox.information(self, "成功", f"表格已成功提取并保存到 {output_excel_path}！")
+            self.result_text.setText(f"表格已保存到 {output_excel_path}")
+            # self.status_label.setText("转换成功！")
         except Exception as e:
             logging.error(f"转换失败：{e}")
             QMessageBox.critical(self, "错误", f"转换失败：{e}")
